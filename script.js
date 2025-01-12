1aef96ee259d6189676bb0a3371948007bc34f54
Mckarov26 <[166902834+mckarov26@users.noreply.github.com]>
2025-01-12 22:44:26 +1300

    modified index.html, style.css and script.js
    - Enhanced the small details in Html
    - Added a fixed height and width on PlayerContainer Selector so it wont change when modified by Javascript
    - Used Javascript Event Delegation to Assign a single event handler to handle all the click events:

--- script.js
+++ script.js
@@ -1,8 +1,42 @@
 const playerScore = document.querySelector("#playerScore");
 const computerScore = document.querySelector("#computerScore");
-const playerDisplay = document.querySelector("#playerDisplay");
-const computerDisplay = document.querySelector("#computerDisplay");
+const playerChoice = document.querySelector("#playerChoice");
+const computerChoice = document.querySelector("#computerChoice");
 const winnerDisplay = document.querySelector("#winnerDisplay");
-const rock = document.querySelector("#rock")
-const paper = document.querySelector("#paper")
-const scissor = document.querySelector("#scissor")
+
+
+// I might not use this anymore insted use the Javascript Event Delegation 
+// const rock = document.querySelector("#rock")
+// const paper = document.querySelector("#paper")
+// const scissor = document.querySelector("#scissor")
+
+
+const buttonContainer = document.querySelector(".btn-container");
+
+//Javascript Event Delegation 
+// Assign a single event handler to handle all the click events:
+
+buttonContainer.addEventListener("click", (event) => {
+    let target = event.target;
+
+    switch(target.id) {
+        case "rock":
+            playerChoice.style.padding = "0";
+            playerChoice.style["font-size"] = "40px";
+            playerChoice.textContent = "✊";
+            
+            break;
+        case "paper":
+            playerChoice.style.padding = "0";
+            playerChoice.style["font-size"] = "40px";
+            playerChoice.textContent = "🫱";
+            break;
+        case "scissor": 
+            playerChoice.style.padding = "0";
+            playerChoice.style["font-size"] = "40px";
+            playerChoice.textContent = "✌️";
+            break;
+    }
+});
+
+
