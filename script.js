edd387e250dfd65bed1ac94f80e6c4f6290dd1c8
Mckarov26 <[166902834+mckarov26@users.noreply.github.com]>
2025-01-12 22:05:21 +1300

    Refactor HTML and JavaScript for Enhanced Element Selection
    - Updated the HTML by adding and removing selectors for better clarity.
    - Stored Element Selectors in JS variables for event listeners.

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-const 
+const playerScore = document.querySelector("#playerScore");
+const computerScore = document.querySelector("#computerScore");
+const playerDisplay = document.querySelector("#playerDisplay");
+const computerDisplay = document.querySelector("#computerDisplay");
+const winnerDisplay = document.querySelector("#winnerDisplay");
+const rock = document.querySelector("#rock")
+const paper = document.querySelector("#paper")
+const scissor = document.querySelector("#scissor")
